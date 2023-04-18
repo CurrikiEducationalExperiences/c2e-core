6132d4766bf38cf02d5df832f5e142f00379241e
Waqar Muneer <i.do.dev@outlook.com>
2023-04-18 23:15:08 +0500

    C2e class update

--- src/interfaces/C2e.ts
+++ src/interfaces/C2e.ts
@@ -1,19 +1,16 @@
-import { C2eContain } from "./C2eContain";
+import { C2eContainer } from "./C2eContainer";
 import { C2eMetadata } from "./C2eMetadata";
 import { JsonLd } from "./JsonLd";
 
 export interface C2e extends JsonLd {
     name: string;
-    c2eType: string;
     c2eMetadata: C2eMetadata;
-    c2eContain: C2eContain;
+    c2eContainer: C2eContainer;
     
     setName(name: string): void;
     getName() : string | undefined;
-    setC2eType(cc2eType: string): void;
-    getC2eType() : string | undefined;
     setC2eMetadata(c2eMetadata: C2eMetadata): void;
     getC2eMetadata(): C2eMetadata | undefined;
-    setC2eContain(c2eContain: C2eContain): void;
-    getC2eContain(): C2eContain | undefined;
+    setC2eContainer(c2eContainer: C2eContainer): void;
+    getC2eContainer(): C2eContainer | undefined;
 }
