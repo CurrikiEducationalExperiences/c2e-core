46d662fd791b430c4f7ed9ae8c6201e7a574f39e
Waqar Muneer <i.do.dev@outlook.com>
2023-05-04 00:35:19 +0500

    C2e writer update :zap:

--- src/index.ts
+++ src/index.ts
@@ -17,43 +17,60 @@ import C2eSourceCodeDocumentLd from "./classes/C2eSourceCodeDocumentLd";
 import C2eWriter from "./writer/C2eWriter";
 import { C2E_DIGITAL_DOCUMENT_TYPE, C2E_RESOURCE_COLLECTION_ID, C2E_COLLECTION_TYPE, C2E_CONTENT_STRING_DATA_TYPE, C2E_DATASET_TYPE, C2E_CONTENT_TYPE_COLLECTION_ID, C2E_SOURCE_CODE_ID } from "./constants";
 
-const c2eWriter = new C2eWriter();
-
 const c2eId = 'xxx';
+const c2eWriter = new C2eWriter(c2eId);
 
 // ==== Define C2E Resources ====
-const c2eResourceCollectionLd = new C2eResourceCollectionLd(C2E_RESOURCE_COLLECTION_ID, C2E_COLLECTION_TYPE);
-const thumbResource = c2eResourceCollectionLd.addC2eResource(new C2eDigitalDocumentLd(c2eId, C2E_DIGITAL_DOCUMENT_TYPE, 'thumbnail.jpg', 'image/jpeg'));
-c2eWriter.createC2eResource('src/resources/thumbnail.jpg', thumbResource);
-const profileResource = c2eResourceCollectionLd.addC2eResource(new C2eDigitalDocumentLd(c2eId, C2E_DIGITAL_DOCUMENT_TYPE, 'profile.jpg', 'image/jpeg'));
-c2eWriter.createC2eResource('src/resources/profile.jpg', profileResource);
+// const c2eResourceCollectionLd = new C2eResourceCollectionLd(C2E_RESOURCE_COLLECTION_ID, C2E_COLLECTION_TYPE);
+// const thumbResource = c2eResourceCollectionLd.addC2eResource(new C2eDigitalDocumentLd(c2eId, C2E_DIGITAL_DOCUMENT_TYPE, 'thumbnail.jpg', 'image/jpeg'));
+c2eWriter.createC2eResource('src/resources/thumbnail.jpg', 'thumbnail.jpg', 'image/jpeg');
+// const profileResource = c2eResourceCollectionLd.addC2eResource(new C2eDigitalDocumentLd(c2eId, C2E_DIGITAL_DOCUMENT_TYPE, 'profile.jpg', 'image/jpeg'));
+c2eWriter.createC2eResource('src/resources/profile.jpg', 'profile.jpg', 'image/jpeg');
 
 // ==== Define C2E Content Types ====
-const c2eContentTypeCollectionLd = new C2eContentTypeCollectionLd(C2E_CONTENT_TYPE_COLLECTION_ID, C2E_COLLECTION_TYPE);
+// const c2eContentTypeCollectionLd = new C2eContentTypeCollectionLd(C2E_CONTENT_TYPE_COLLECTION_ID, C2E_COLLECTION_TYPE);
 // Define 'Project' Content Type
-const c2eProjectTitle = new C2eContentTypeAttributeLd('title', C2E_CONTENT_STRING_DATA_TYPE);
+/* const c2eProjectTitle = new C2eContentTypeAttributeLd('title', C2E_CONTENT_STRING_DATA_TYPE);
 const c2eProjectDescription = new C2eContentTypeAttributeLd('description', C2E_CONTENT_STRING_DATA_TYPE);
 const c2eProjectAttributes = [c2eProjectTitle, c2eProjectDescription];
 const c2eProjectContentType = new C2eContentTypeLd(c2eId, 'project', c2eProjectAttributes);
 c2eContentTypeCollectionLd.addC2eContentType(c2eProjectContentType);
+ */
+
+c2eWriter.createC2eContentType('project', [
+    {property: "title", type: C2E_CONTENT_STRING_DATA_TYPE}, 
+    {property: "description", type: C2E_CONTENT_STRING_DATA_TYPE}, 
+]);
+
+c2eWriter.createC2eContentType('playlist', [
+    {property: "title", type: C2E_CONTENT_STRING_DATA_TYPE}, 
+    {property: "description", type: C2E_CONTENT_STRING_DATA_TYPE}, 
+]);
+
+c2eWriter.createC2eContentType('activity', [
+    {property: "title", type: C2E_CONTENT_STRING_DATA_TYPE},
+    {property: "description", type: C2E_CONTENT_STRING_DATA_TYPE},
+]);
+
 // Define 'Playlist' Content Type
-const c2ePlaylistTitle = new C2eContentTypeAttributeLd('title', C2E_CONTENT_STRING_DATA_TYPE);
+/* const c2ePlaylistTitle = new C2eContentTypeAttributeLd('title', C2E_CONTENT_STRING_DATA_TYPE);
 const c2ePlaylistDescription = new C2eContentTypeAttributeLd('description', C2E_CONTENT_STRING_DATA_TYPE);
 const c2ePlaylistContentType = new C2eContentTypeLd(c2eId, 'playlist', [c2ePlaylistTitle, c2ePlaylistDescription]);
 c2eContentTypeCollectionLd.addC2eContentType(c2ePlaylistContentType);
+ */
 // Define 'Activity' Content Type
-const c2eActivityTitle = new C2eContentTypeAttributeLd('title', C2E_CONTENT_STRING_DATA_TYPE);
+/* const c2eActivityTitle = new C2eContentTypeAttributeLd('title', C2E_CONTENT_STRING_DATA_TYPE);
 const c2eActivityDescription = new C2eContentTypeAttributeLd('description', C2E_CONTENT_STRING_DATA_TYPE);
 const c2eActivityContentType = new C2eContentTypeLd(c2eId, 'activity', [c2eActivityTitle, c2eActivityDescription]);
 c2eContentTypeCollectionLd.addC2eContentType(c2eActivityContentType);
-
+ */
 // ==== Define C2E Reader Plugin entry point code ====
 const htmlDocument = new C2eSourceCodeDocumentLd(c2eId, 'index.html', 'text/html');
 const javascriptDocument = new C2eSourceCodeDocumentLd(c2eId, 'index.js', 'application/javascript');
 const c2eSourceCode = new C2eSourceCodeLd(C2E_SOURCE_CODE_ID, C2E_COLLECTION_TYPE, htmlDocument, javascriptDocument);
 
 //==== Define C2E Container ====
-const C2eContainer = new C2eContainerLd(c2eResourceCollectionLd, c2eContentTypeCollectionLd, c2eSourceCode);
+const C2eContainer = new C2eContainerLd(c2eWriter.getResourceCollection(), c2eWriter.getc2eContentTypeCollection(), c2eSourceCode);
 
 // ==== Define C2E Metadata ====
 const c2eMdGeneralLd = new C2eMdGeneralLd('c2ens:c2eid-xxx/metadata/general', C2E_DATASET_TYPE, 'My Project C2E', 'This is CurrikiStudio Project as a C2e', ["Education", "Curriculum", "Curriki", "Project"]);
--- src/writer/C2eWriter.ts
+++ src/writer/C2eWriter.ts
@@ -1,15 +1,42 @@
+import C2eDigitalDocumentLd from "../classes/C2eDigitalDocumentLd";
+import C2eResourceCollectionLd from "../classes/C2eResourceCollectionLd";
 import C2eDigitalDocument from "../interfaces/C2eDigitalDocument";
+import C2eContentTypeCollectionLd from "../classes/C2eContentTypeCollectionLd";
 import C2e from "../interfaces/C2e";
+import { C2E_DIGITAL_DOCUMENT_TYPE, C2E_RESOURCE_COLLECTION_ID, C2E_COLLECTION_TYPE, C2E_CONTENT_STRING_DATA_TYPE, C2E_DATASET_TYPE, C2E_CONTENT_TYPE_COLLECTION_ID, C2E_SOURCE_CODE_ID } from "../constants";
 
 export default class C2eWriter {
-    c2eResourcesToCreate: Array<{source: string, c2eResource: C2eDigitalDocument}> = [];
+    c2eId: string;
+    c2eResourceCollectionLd: C2eResourceCollectionLd;
+    c2eResourcesToCreate: Array<{sourceFilePath: string, c2eResource: C2eDigitalDocument}>;
+    c2eContentTypeCollectionLd: C2eContentTypeCollectionLd;
+
+    constructor(c2eId: string) {
+        this.c2eId = c2eId;
+        this.c2eResourceCollectionLd = new C2eResourceCollectionLd(C2E_RESOURCE_COLLECTION_ID, C2E_COLLECTION_TYPE);
+        this.c2eResourcesToCreate = [];
+        this.c2eContentTypeCollectionLd = new C2eContentTypeCollectionLd(C2E_CONTENT_TYPE_COLLECTION_ID, C2E_COLLECTION_TYPE);
+    }
 
     createC2e(c2e: C2e) {
         console.log('create C2e', c2e);
     }
 
-    createC2eResource(source: string, c2eResource: C2eDigitalDocument): C2eDigitalDocument {
-        this.c2eResourcesToCreate.push({source, c2eResource});
-        return c2eResource;
+    createC2eResource(sourceFilePath: string, targetFilePath: string, MIMEType: string): void {
+        const c2eResource: C2eDigitalDocumentLd = this.c2eResourceCollectionLd.addC2eResource(new C2eDigitalDocumentLd(this.c2eId, C2E_DIGITAL_DOCUMENT_TYPE, targetFilePath, MIMEType));
+        this.c2eResourcesToCreate.push({sourceFilePath, c2eResource});
+    }
+
+    createC2eContentType(contentTypeName: string, contentTypeAttributes: Array<{property: string, type: string}>): void {
+        //this.c2eContentTypeCollectionLd.addC2eContentType(contentType, contentDataType);
+    }
+
+    //******** temp **********
+    getResourceCollection(): C2eResourceCollectionLd {
+        return this.c2eResourceCollectionLd;
+    }
+
+    getc2eContentTypeCollection(): C2eContentTypeCollectionLd {
+        return this.c2eContentTypeCollectionLd;
     }
 }
